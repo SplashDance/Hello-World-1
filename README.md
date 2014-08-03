530b98a976474b00ff8a3157bf4c86bd7edc8759
SplashDance <stevesargentjr@hotmail.com>
2014-08-03 05:06:04 -0400

    Changes made for FUTURE version

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 ##README
 
-Consider me *read*.
+This is the FUTURE version of Hello-World.py
+
+
+You haven't read me yet...
--- hola.py
+++ hola.py
@@ -1,5 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os, random
+
+try:
+    from tkinter import *
+except ImportError:
+    from Tkinter import *    
+
 def main():
-    print("¡Hola, él mundo!")
+    assert os.path.exists("names.txt"), 'Cannot find names.txt.'
+    namesFileRead = open("names.txt", 'r')
+    content = namesFileRead.readlines()
+    namesFileRead.close()
+    name = content[random.randint(0, len(content) - 1)].rstrip('\r\n')
+    
+    root = Tk()
+    
+    w = Label(root, text = "¡Hola, " + name + "!")
+    w.pack()
+    
+    root.mainloop()
 
 #comments
 if __name__ == '__main__':
